a3f6cc407f656943124f588b4cd51d3a405d79a5
Marcelo <marcelolopezdelerma@gmail.com>
2021-10-16 14:05:27 +0200

    Coleccionable Script

---
+++ Assets/Scripts/Coleccionable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coleccionable : MonoBehaviour
+{
+    void OnCollisionEnter(Collision obj)
+    {
+        //Check to see if the tag on the collider is equal to Enemy
+        if (obj.collider.tag == "Player")
+        {
+            Debug.Log("Triggered by Enemy");
+        }
+    }
+}
